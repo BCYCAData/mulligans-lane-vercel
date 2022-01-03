f32f5c82fcac9bfbbbd56ec11980288f2d7078cb
Alan Keown <AlanKeown@southernphone.com.au>
2022-01-03 14:20:47 +1100

    Still more

--- src/routes/auth/resetpassword.svelte
+++ src/routes/auth/resetpassword.svelte
@@ -1,5 +1,5 @@
 <script>
-	import supabaseConnection from '$lib/db';
+	import supabaseConnection from '$lib/dbClient';
 
 	export async function surveyResult({ fetch }) {
 		let survey;
@@ -35,7 +35,9 @@
 	}
 </script>
 
-<div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+<div
+	class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
+>
 	<div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
 		<form on:submit|preventDefault={resetPassword}>
 			<h1 class="mb-8 text-3xl text-center">Reset Password</h1>
