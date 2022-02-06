1ca801d93bed7016575fa8acfb61bd9684d5a0b1
Alan Keown <AlanKeown@southernphone.com.au>
2022-02-06 13:26:13 +1100

    Basic challenge working

--- src/components/form/AddressChallenge.svelte
+++ src/components/form/AddressChallenge.svelte
@@ -1,4 +1,32 @@
-<form action="/api/data/property" method="post">
+<script>
+	import { PIP } from '$lib/spatialUtils';
+	import communityPolygons from '$lib/geojsonData/communities';
+
+	let streetaddress;
+	let suburb;
+	async function submitForm() {
+		const response = await fetch('/api/data/property', {
+			method: 'POST',
+			body: JSON.stringify({ streetaddress, suburb })
+		});
+		console.log(response);
+		if (response.status === 400) {
+			console.log('Error');
+		} else if (response.status === 404) {
+			console.log('Address not found');
+		} else {
+			let results = await response.json();
+			const point = [Math.round(results.X), Math.round(results.Y)];
+			for (let i = 0; i < communityPolygons.features.length; i++) {
+				let polygon = communityPolygons.features[i].geometry.coordinates[0];
+				let ok = PIP(polygon[0], point);
+				console.log(ok);
+			}
+		}
+	}
+</script>
+
+<form on:submit|preventDefault={submitForm}>
 	<input
 		id="streetaddress"
 		type="text"
@@ -7,6 +35,7 @@
 		required
 		placeholder="Street Address"
 		autocomplete="address-line1"
+		bind:value={streetaddress}
 	/>
 	<div class="flex justify-between">
 		<input
@@ -17,6 +46,7 @@
 			required
 			placeholder="Suburb"
 			autocomplete="address-level2"
+			bind:value={suburb}
 		/>
 		<button
 			type="submit"
--- src/routes/api/data/property.js
+++ src/routes/api/data/property.js
@@ -1,7 +1,8 @@
 export async function post({ request }) {
-	const body = await request.formData();
-	const street = body.get('streetaddress').toUpperCase();
-	const suburb = body.get('suburb').toUpperCase();
+	// const body = await request.formData();
+	const body = await request.json();
+	const street = body.streetaddress.toUpperCase();
+	const suburb = body.suburb.toUpperCase();
 	let addressString = `${street} ${suburb}`;
 	const addressURL =
 		'https://portal.spatial.nsw.gov.au/server/rest/services/NSW_Geocoded_Addressing_Theme/MapServer/1/query?where=';
@@ -9,21 +10,35 @@ export async function post({ request }) {
 	let addressQuery = `address='${addressString}'`;
 	const addressQueryParams = '&outFields=*&featureEncoding=esriDefault&f=pjson';
 	let geocodingFetchUrl = `${addressURL}${addressQuery}${addressQueryParams}`;
-	let geocodeResponse = await fetch(geocodingFetchUrl);
-	let geocodeData = await geocodeResponse.json();
-	let pointGeometryString = `${geocodeData.features[0].geometry.x}'%2C'${geocodeData.features[0].geometry.y}`;
-	const propertyURL =
-		'https://portal.spatial.nsw.gov.au/server/rest/services/NSW_Land_Parcel_Property_Theme/FeatureServer/12/query?';
-	let propertyQuery = `where=&objectIds=&time=&geometry=${pointGeometryString}&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects`;
-	const propertyQueryParams =
-		'&distance=&units=esriSRUnit_Meter&relationParam=&outFields=&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnExceededLimitFeatures=false&quantizationParameters=&returnCentroid=false&sqlFormat=none&resultType=&featureEncoding=esriDefault&datumTransformation=&f=geojson';
-	let propertyFetchUrl = `${propertyURL}${propertyQuery}${propertyQueryParams}`;
-	let response = await fetch(propertyFetchUrl);
+	// console.log(geocodingFetchUrl);
+	let response = await fetch(geocodingFetchUrl);
 	let data = await response.json();
-	console.log(data);
+	// console.log(data);
+	// let pointGeometryString = `${data.features[0].geometry.x}'%2C'${data.features[0].geometry.y}`;
+	// const propertyURL =
+	// 	'https://portal.spatial.nsw.gov.au/server/rest/services/NSW_Land_Parcel_Property_Theme/FeatureServer/12/query?';
+	// let propertyQuery = `where=&objectIds=&time=&geometry=${pointGeometryString}&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects`;
+	// const propertyQueryParams =
+	// 	'&distance=&units=esriSRUnit_Meter&relationParam=&outFields=&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false&returnExceededLimitFeatures=false&quantizationParameters=&returnCentroid=false&sqlFormat=none&resultType=&featureEncoding=esriDefault&datumTransformation=&f=geojson';
+	// let propertyFetchUrl = `${propertyURL}${propertyQuery}${propertyQueryParams}`;
+	// let response = await fetch(propertyFetchUrl);
+	// let data = await response.json();
+	if (data.features?.length) {
+		return {
+			status: 200,
+			body: {
+				X: data.features[0].geometry.x,
+				Y: data.features[0].geometry.y
+			}
+		};
+	} else if (data.features) {
+		return {
+			status: 404,
+			body: {}
+		};
+	}
 	return {
-		status: 200,
-		// headers: { location: '/about' }
-		body: { address: addressString, addressURL: addressURL }
+		status: 400,
+		body: {}
 	};
 }
