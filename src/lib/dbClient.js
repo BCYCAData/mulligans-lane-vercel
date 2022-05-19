55c8f9ad27951592a326a4a55e6d7f6ea56e3b69
Alan Keown <bcycadata@outlook.com>
2022-05-20 09:52:34 +1000

    Maybe - Sveltekit rollback

--- src/lib/dbClient.js
+++ src/lib/dbClient.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+// import electron from 'electron';
+// const { app, session, BrowserWindow } = electron;
 
 export const supabaseRedirectBase =
 	import.meta.env.VITE_SUPABASE_REDIRECT_URL_BASE.toString();
