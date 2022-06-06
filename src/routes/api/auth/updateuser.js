af9b419df4112cd278b005c6a5241bbe586c00f7
Alan Keown <bcycadata@outlook.com>
2022-06-06 14:27:13 +1000

    FormBody

--- src/routes/api/auth/updateuser.js
+++ src/routes/api/auth/updateuser.js
@@ -1,9 +1,10 @@
 import { db } from '$lib/dbClient';
+import { getFormBody } from '$lib';
 
 export async function post({ request }) {
 	const _session = db.auth.session();
 	const token = _session?.access_token;
-	const body = await request.formData();
+	const body = await getFormBody(request.formData());
 	password = body.get('password');
 	db.auth.setAuth(token);
 	const { data, error } = await db.auth.update({ password: 'password' });
--- src/routes/api/userdata/survey.js
+++ src/routes/api/userdata/survey.js
@@ -1,4 +1,5 @@
 import { db } from '$lib/dbClient';
+import { getFormBody } from '$lib';
 
 const check = [
 	'first_name',
@@ -124,18 +125,6 @@ export async function post({ request }) {
 	return {
 		body: { surveyAnswers }
 	};
-}
-function getFormBody(body) {
-	return [...body.entries()].reduce((data, [k, v]) => {
-		let value = v;
-		if (value === 'true') value = true;
-		if (value === 'false') value = false;
-		if (value === '') value = null;
-		if (k in data)
-			data[k] = Array.isArray(data[k]) ? [...data[k], value] : [data[k], value];
-		else data[k] = value;
-		return data;
-	}, {});
 }
 
 function setMissing(data) {
