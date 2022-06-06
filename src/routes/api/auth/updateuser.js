e77ff2c5e903b2055ca730d7d1be9505853461f4
Alan Keown <bcycadata@outlook.com>
2022-06-06 14:49:22 +1000

    IX

--- src/routes/api/auth/updateuser.js
+++ src/routes/api/auth/updateuser.js
@@ -2,14 +2,9 @@ import { db } from '$lib/dbClient';
 import { getFormBody } from '$lib/utils';
 
 export async function post({ request }) {
-	const _session = db.auth.session();
-	console.log(_session);
-	const token = _session?.access_token;
-	console.log(token);
 	const body = await request.formData();
 	const formBody = await getFormBody(body);
 	password = formBody.password;
-	db.auth.setAuth(token);
 	const { data, error } = await db.auth.update({ password: 'password' });
 	console.log(data);
 	if (error) {
